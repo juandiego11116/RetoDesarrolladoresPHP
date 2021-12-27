4a05ac7796e4608d92d70b3c22b3ed4fbd8a310e
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2021-12-27 08:07:01 -0500

    feat add menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,4 +2,13 @@
     <a class="nav-link" href="/">
         <i class=" fas fa-building"></i><span>Dashboard</span>
     </a>
+    <a class="nav-link" href="/users">
+        <i class=" fas fa-users"></i><span>Users</span>
+    </a>
+    <a class="nav-link" href="/roles">
+        <i class=" fas fa-user-lock"></i><span>Roles</span>
+    </a>
+    <a class="nav-link" href="/product">
+        <i class=" fas fa-layer-group"></i><span>Products</span>
+    </a>
 </li>
