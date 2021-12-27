5d69e3c98eb4f2808849fb0ae3b40d3338ee783b
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2021-12-27 08:05:03 -0500

    feat: Seeder Permissions

---
+++ database/seeders/SeederTablePermissions.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class SeederTablePermissions extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permissions = [
+            //roles permissions
+            'showRole',
+            'createRole',
+            'editRole',
+            'deleteRole',
+            //product permissions
+            'showProduct',
+            'createProduct',
+            'editProduct',
+            'deleteProduct',
+
+        ];
+        foreach ($permissions as $permission){
+            Permission::create(['name'=>$permission]);
+        }
+    }
+}
