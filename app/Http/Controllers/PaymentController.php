12da75233f8c59b76086bef29b01b83ab46cab49
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-01-20 17:27:49 -0500

    feat: add create purchase

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Purchase;
 use App\Playtopay\PaymentGatewayContract;
 use Illuminate\Http\Request;
 
@@ -10,14 +11,20 @@ class PaymentController extends Controller
 
     public function store(Request $request)
     {
-
         $paymentGateway = app()->make(PaymentGatewayContract::class, $request->only('total', 'reference', 'description'));
         $response = $paymentGateway->createSession();
+        $data = [
+            'id_product' => $request['id_product'],
+            'id_request' => $response['requestId'],
+            'price' => $request['price'],
+            'amount' => $request['amount'],
+            'status' => 'pending',
+        ];
+        Purchase::create($data);
+
 
         return redirect()->away($response['processUrl']);
     }
-    public function index(Request $request){
 
-    }
 
 }
