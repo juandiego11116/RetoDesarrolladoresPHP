e046e14cef4f549c4293187ce6d9ec88b7828479
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-01-20 17:31:23 -0500

    feat: create componen cart-button

--- public/css/app.css
+++ public/css/app.css
@@ -1,1 +1,8 @@
 require('./bootstrap');
+import {createApp} from "vue";
+import CartButton from "../components/CartButton";
+createApp({
+    components: {
+        'cart-button': CartButton
+    }
+}).mount('#app');
