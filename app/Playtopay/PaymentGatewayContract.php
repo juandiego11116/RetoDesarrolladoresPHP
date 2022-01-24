4a596b5f8b4b771202158e3153ff0a17fb317643
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-01-23 23:48:51 -0500

    Refactory: name Placetopay

--- app/Playtopay/PaymentGatewayContract.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Playtopay;
-
-interface PaymentGatewayContract
-{
-    public function createSession():array;
-
-}
--- config/playtopay.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'login' => env('PLACETOPAY_LOGIN'),
-    'secretKey' => env('PLACETOPAY_SECRET_KEY'),
-    'uri' => env('PLACETOPAY_URI')
-
-];
