c7075b772eca4b728a1ce500ba0b31f913b86626
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-03-03 19:06:55 -0500

    refactory: integration with categories table

--- app/Models/product.php
+++ app/Models/product.php
@@ -4,16 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function categories():HasOne
+    {
+        return $this->hasOne(Category::class);
+    }
+
     protected $fillable = [
         'name',
         'price',
         'stock_number',
-        'category',
+        'id_category',
         'description',
         'photo',
+        'visible',
     ];
+
+    public function getPhotoAttribute():string
+    {
+        return $this->attributes['photo'] = '/storage/'.$this->attributes['photo'];
+    }
+
 }
