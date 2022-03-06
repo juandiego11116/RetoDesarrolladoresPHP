4cd558df221bad0f10e2f68abbd6abf08048b93f
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-03-06 10:24:30 -0500

    refactory: add id_countries and document_types

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,12 +26,12 @@ class UserController extends Controller
         $this->middleware('permission:delete-user', ['only'=>['destroy']]);
     }
 
-    public function search(Request $request):View
+    public function search(Request $request): View
     {
         $users = User::where('name', 'like', '%'. $request->text . '%')->take(10)->get();
         return view('users.index', compact('users'));
     }
-    public function index(Request $request):View
+    public function index(Request $request): View
     {
         $text = trim($request->get('text'));
         $users = DB::table('users')
@@ -44,76 +44,127 @@ class UserController extends Controller
         return view('users.index', compact('users', 'text'));
     }
 
-    public function create():View
+    public function create(): View
     {
+        $documentTypes =DB::table('document_types')
+            ->select('id', 'name')
+            ->get();
+
+        $countries = DB::table('countries')
+            ->select('id', 'name')
+            ->get();
+
         $roles = Role::pluck('name', 'name')->all();
-        return view('users.create', compact('roles'));
+        return view('users.create', compact('roles', 'countries', 'documentTypes'));
     }
 
-    public function store(Request $request):RedirectResponse
+    public function store(Request $request): RedirectResponse
     {
         //cambiar  a form request
         $this->validate($request, [
             'name' => 'required',
             'last_name' => 'required',
-            'document_type' => 'required',
+            'id_document_type' => 'required',
             'document' => 'required',
-            'country' => 'required',
+            'id_country' => 'required',
             'address' => 'required',
             'phone_number' => 'required',
             'email' => 'required|email|unique:users,email',
             'password' => 'required|same:confirm-password',
             'roles' => 'required'
         ]);
-        $input = $request->all();
-        $input['password'] = Hash::make($input['password']);
-        $user = User::create($input);
+
+        $documentTypes = DB::table('document_types')
+            ->select('id')
+            ->where('name', '=', $request->id_document_type)
+            ->get();
+
+        $countries = DB::table('countries')
+            ->select('id')
+            ->where('name', '=', $request->id_country)
+            ->get();
+
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->last_name = $request->input('last_name');
+        $user->id_document_type = $documentTypes[0]->id;
+        $user->document = $request->input('document');
+        $user->id_country = $countries[0]->id;
+        $user->address = $request->input('address');
+        $user->phone_number = $request->input('phone_number');
+        $user->email = $request->input('email');
+        $user->password = Hash::make($request->input('password'));
+        $user->save();
         $user->assignRole($request->input('roles'));
+
+
         return redirect()->route('users.index');
     }
 
-    public function edit($id):View
+    public function edit($id): View
     {
         $user =  User::find($id);
         $roles = Role::pluck('name', 'name')->all();
         $userRole = $user->roles->pluck('name', 'name')->all();
-        return view('users.edit', compact('user', 'roles', 'userRole'));
+        $documentTypes =DB::table('document_types')
+            ->select('id', 'name')
+            ->get();
+
+        $countries = DB::table('countries')
+            ->select('id', 'name')
+            ->get();
+        return view('users.edit', compact('user', 'roles', 'userRole', 'countries', 'documentTypes'));
     }
 
-    public function update(Request $request, $id):RedirectResponse
+    public function update(Request $request, $id): RedirectResponse
     {
         $this->validate($request, [
             'name' => 'required',
             'last_name' => 'required',
-            'document_type' => 'required',
+            'id_document_type' => 'required',
             'document' => 'required',
-            'country' => 'required',
+            'id_country' => 'required',
             'address' => 'required',
             'phone_number' => 'required',
             'email' => 'required|email|unique:users,email,'.$id,
             'password' => 'same:confirm-password',
             'roles' => 'required'
         ]);
-        $input = $request->all();
-        if (!empty($input['password'])) {
-            $input['password'] = Hash::make($input['password']);
-        } else {
-            $input = Arr::except($input, array('password'));
-        }
-        $user = User::find($id);
-        $user->update($input);
-        DB::table('model_has_roles')->where('model_id', $id)->delete();
+        $documentTypes = DB::table('document_types')
+            ->select('id')
+            ->where('name', '=', $request->id_document_type)
+            ->get();
 
-        $user->assignRole($request->input('roles'));
+        $countries = DB::table('countries')
+            ->select('id')
+            ->where('name', '=', $request->id_country)
+            ->get();
+
+        $input = $request->all();
+        if (!empty($request->password)) {
+            $request->password = Hash::make($request->password);
+        } else {
+            $request = Arr::except($input, array('password'));
+        }
+
+        $request['id_document_type'] = $documentTypes[0]->id;
+        $request['id_country'] = $countries[0]->id;
+
+        $user = User::find($id);
+        $user->update($request);
+
+        DB::table('model_has_roles')->where('model_id', $id)->delete();
+        $user->assignRole($request['roles']);
+
         return redirect()->route('users.index');
     }
 
-    public function destroy($id):RedirectResponse
+    public function destroy($id): RedirectResponse
     {
         User::find($id)->delete();
         return redirect()->route('users.index');
     }
-    public function getUsers(Request $request):JsonResponse
+    public function getUsers(Request $request): JsonResponse
     {
         $filter = $request->search;
 
