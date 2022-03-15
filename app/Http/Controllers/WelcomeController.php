37e0a6270c05ecf9c21c328629911b4456d4e037
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-03-15 17:17:36 -0500

    feat: create welcome controller

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function index()
+    {
+        $products = Product::inRandomOrder()
+            ->with([
+                'categories' => function ($query) {
+                    $query->select('id', 'name');
+                }
+            ])->paginate(8);
+
+
+        return view('welcome', compact('products'));
+    }
+}
