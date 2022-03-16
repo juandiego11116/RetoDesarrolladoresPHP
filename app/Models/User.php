1547fc5a2e745bb7f4b923649ffa9ca51c9ff82b
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-03-16 16:50:48 -0500

    refactory: add scopeSearch

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -17,6 +19,13 @@ class User extends Authenticatable implements MustVerifyEmail
     use Notifiable;
     use HasRoles;
 
+    public function scopeSearch(Builder $query, $search): Builder
+    {
+        if ($search) {
+            return $query->where('name', 'LIKE', "%$search%");
+        }
+        return $query;
+    }
     public function countries(): HasOne
     {
         return $this->hasOne(Country::class);
