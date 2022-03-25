0b890df96b851df9f5886fce09b3b7513a4ff620
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-03-24 19:14:40 -0500

    refactory: change model for eloquent

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -2,34 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\Purchase;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
+use phpDocumentor\Reflection\Types\Void_;
 
 class PurchaseController extends Controller
 {
-    public function __construct()
+    /*public function __construct()
     {
         $this->middleware('permission:show-product|create-product|edit-product|delete-product', ['only'=>['index']]);
         $this->middleware('permission:create-product', ['only'=>['create','store']]);
         $this->middleware('permission:edit-product', ['only'=>['edit','update']]);
         $this->middleware('permission:delete-product', ['only'=>['destroy']]);
-    }
+    }*/
 
-    public function index(Request $request): View
+    /*public function index(Request $request): View
     {
         $text = trim($request->get('text'));
-        $products = DB::table('products')
-            ->select('id', 'name', 'price', 'stock_number', 'category')
-            ->where('name', 'LIKE', '%'.$text.'%')
-            ->orWhere('price', 'LIKE', '%'.$text.'%')
-            ->orWhere('stock_number', 'LIKE', '%'.$text.'%')
-            ->orWhere('category', 'LIKE', '%'.$text.'%')
-            ->orderBy('name', 'asc')
-            ->paginate(5);
+        $products = Product::with([
+            'categories' => function ($query) {
+                $query->select('id', 'name');
+            }
+        ])->orderBy('id_category', 'asc')
+            ->search($text)
+            ->paginate(5, ['id', 'name', 'price', 'photo', 'stock_number', 'visible', 'id_category']);
+
         return view('purchases.index', compact('products', 'text'));
-    }
+    }*/
+
     public function create(Request $request): View
     {
         $input = $request->get('product');
@@ -43,10 +46,48 @@ class PurchaseController extends Controller
 
         return view('purchases.cart', compact('product', 'input', 'amount'));
     }
+
+    public function show(int $productId)
+    {
+
+        $product = Product::with([
+            'categories' => function ($query) {
+                $query->select('id', 'name');
+            }
+        ])->where('id', $productId)
+          ->get(['id', 'name', 'price', 'photo', 'stock_number', 'visible', 'id_category']);
+
+        return view('purchases.show', compact('product'));
+    }
+    public function store(Request $request)
+    {
+
+        $text = trim($request->get('id'));
+
+        $product = Product::with([
+            'categories' => function ($query) {
+                $query->select('id', 'name');
+            }
+        ])
+
+            ->get( 'id', 'name', 'price', 'photo', 'stock_number', 'visible', 'id_category');
+        dd($product);
+
+
+        return back();
+    }
+
     public function addToCart(Request $request): View
     {
-        $product = ProductController::find($request->product);
 
-        return view('purchases.cart', compact('product'));
+        $products = Product::with([
+            'categories' => function ($query) {
+                $query->select('id', 'name');
+            }
+        ])->where('id', '=', $request->query('productId'))
+            ->orderBy('id_category', 'asc')
+            ->paginate(5, ['id', 'name', 'price', 'photo', 'stock_number', 'visible', 'id_category']);
+
+        return view('purchases.cart', compact('products'));
     }
 }
