7f13da969ae958f04db3f7d9cfb6055ae0f598a8
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-04-12 10:11:51 -0500

    style: remove comment and space, chance name variable

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Models\Purchase;
+use App\Placetopay\PaymentGatewayContract;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
@@ -11,45 +12,22 @@ use phpDocumentor\Reflection\Types\Void_;
 
 class PurchaseController extends Controller
 {
-    /*public function __construct()
-    {
-        $this->middleware('permission:show-product|create-product|edit-product|delete-product', ['only'=>['index']]);
-        $this->middleware('permission:create-product', ['only'=>['create','store']]);
-        $this->middleware('permission:edit-product', ['only'=>['edit','update']]);
-        $this->middleware('permission:delete-product', ['only'=>['destroy']]);
-    }*/
-
-    /*public function index(Request $request): View
-    {
-        $text = trim($request->get('text'));
-        $products = Product::with([
-            'categories' => function ($query) {
-                $query->select('id', 'name');
-            }
-        ])->orderBy('id_category', 'asc')
-            ->search($text)
-            ->paginate(5, ['id', 'name', 'price', 'photo', 'stock_number', 'visible', 'id_category']);
-
-        return view('purchases.index', compact('products', 'text'));
-    }*/
-
     public function create(Request $request): View
     {
         $input = $request->get('product');
-        $amount = $request->get('amount');
+        $quantity = $request->get('amount');
 
         $product = DB::table('products')
             ->select('id', 'name', 'price')
-            ->where('id', '=', $input)
+            ->where('id', $input)
             ->orderBy('name', 'asc')
             ->paginate(5);
 
-        return view('purchases.cart', compact('product', 'input', 'amount'));
+        return view('purchases.cart', compact('product', 'input', 'quantity'));
     }
 
     public function show(int $productId)
     {
-
         $product = Product::with([
             'categories' => function ($query) {
                 $query->select('id', 'name');
@@ -61,7 +39,6 @@ class PurchaseController extends Controller
     }
     public function store(Request $request)
     {
-
         $text = trim($request->get('id'));
 
         $product = Product::with([
@@ -70,16 +47,31 @@ class PurchaseController extends Controller
             }
         ])
 
-            ->get( 'id', 'name', 'price', 'photo', 'stock_number', 'visible', 'id_category');
+            ->get('id', 'name', 'price', 'photo', 'stock_number', 'visible', 'id_category');
         dd($product);
 
 
         return back();
     }
 
+    public function history(Request $request): View
+    {
+        $text = trim($request->get('text'));
+
+        $purchases = DB::table('purchases')
+            ->select('id', 'reference', 'total', 'status')
+            ->where('total', 'LIKE', '%'.$text.'%')
+            ->orWhere('reference', 'LIKE', '%'.$text.'%')
+            ->orWhere('status', 'LIKE', '%'.$text.'%')
+            ->orderBy('status', 'asc')
+            ->paginate(5);
+
+
+        return view('purchases.history', compact('purchases', 'text'));
+    }
+
     public function addToCart(Request $request): View
     {
-
         $products = Product::with([
             'categories' => function ($query) {
                 $query->select('id', 'name');
