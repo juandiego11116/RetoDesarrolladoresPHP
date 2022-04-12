4f0f723c85e52402ad555d2533bccc0d51fcd038
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-04-12 10:12:24 -0500

    stye: organice function

--- app/Models/product.php
+++ app/Models/product.php
@@ -6,24 +6,11 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
     use HasFactory;
-
-    public function categories(): BelongsTo
-    {
-        return $this->belongsTo(Category::class, 'id_category');
-    }
-
-    public function scopeSearch(Builder $query, $search): Builder
-    {
-        if ($search) {
-            return $query->where('name', 'LIKE', "%$search%");
-        }
-        return $query;
-    }
-
 
     protected $fillable = [
         'name',
@@ -34,6 +21,24 @@ class Product extends Model
         'photo',
         'visible',
     ];
+
+    public function categories(): BelongsTo
+    {
+        return $this->belongsTo(Category::class, 'id_category');
+    }
+
+    public function purchases(): BelongsToMany
+    {
+        return $this->belongsToMany(Purchase::class, 'purchase_product', 'purchase_id', 'product_id');
+    }
+
+    public function scopeSearch(Builder $query, $search): Builder
+    {
+        if ($search) {
+            return $query->where('name', 'LIKE', "%$search%");
+        }
+        return $query;
+    }
 
     public function getPhotoAttribute(): string
     {
