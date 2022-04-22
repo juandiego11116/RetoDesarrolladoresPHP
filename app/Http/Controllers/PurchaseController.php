c589268dac7e2193dee3b4cde7211d99661bde4e
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-04-22 16:36:02 -0500

    refactory: add function show and add type function

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use App\Models\Purchase;
 use App\Placetopay\PaymentGatewayContract;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
@@ -26,18 +27,24 @@ class PurchaseController extends Controller
         return view('purchases.cart', compact('product', 'input', 'quantity'));
     }
 
-    public function show(int $productId)
+    public function show( int $purchase):View
     {
-        $product = Product::with([
-            'categories' => function ($query) {
-                $query->select('id', 'name');
-            }
-        ])->where('id', $productId)
-          ->get(['id', 'name', 'price', 'photo', 'stock_number', 'visible', 'id_category']);
 
-        return view('purchases.show', compact('product'));
+        $purchases = DB::table('purchases')
+            ->select('id','id_request', 'total', 'status', 'deduct_from_stock', 'reference')
+            ->where('id', $purchase)
+            ->get();
+        $reference = $purchases[0]->reference;
+        $products = DB::table('purchase_product')
+            ->join('products', 'purchase_product.product_id', '=', 'products.id')
+            ->select('products.id','products.name', 'purchase_product.amount', 'purchase_product.subtotal', 'purchase_product.price', 'products.stock_number')
+            ->where('purchase_id',  $purchases[0]->id)
+            ->get();
+
+        return view('purchases.show', compact('purchases', 'products', 'reference'));
+
     }
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         $text = trim($request->get('id'));
 
