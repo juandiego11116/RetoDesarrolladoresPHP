7835dbf697a00c6eab0668db0483c689c0c5f822
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-04-26 19:12:01 -0500

    refactory: update last_name, phone_number and email_verified_at

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,14 +13,14 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
-            'lastName' => $this->faker->lastName(),
+            'last_name' => $this->faker->lastName(),
             'id_document_type' => '1',
             'document' => $this->faker->numberBetween(0000000000, 9999999999),
             'id_country' => '1',
             'address' => $this->faker->address(),
-            'phoneNumber' => $this->faker->phoneNumber(),
+            'phone_number' => $this->faker->phoneNumber(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'email_verified_at' => '2022-04-23 10:47:44',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
