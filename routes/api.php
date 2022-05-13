9963419993229f64cf0239641ee6d3e787c33e9e
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-05-13 16:29:09 -0500

    feat: create route for api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/products', [App\Http\Controllers\Api\ProductController::class, 'index']);
+Route::post('/products/store', [App\Http\Controllers\Api\ProductController::class, 'store']);
+Route::patch('/products/update/{id}', [App\Http\Controllers\Api\ProductController::class, 'update']);
+Route::delete('/products/delete/{id}', [App\Http\Controllers\Api\ProductController::class, 'destroy']);
