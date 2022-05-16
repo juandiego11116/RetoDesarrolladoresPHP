82bf87dfa210aed26f90b50d7ebbb0cd8cca1135
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-05-15 21:38:09 -0500

    refactory: delete function newProductImport and create productImport

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Exports\ProductsExport;
 use App\Exports\SalesExport;
-use App\Imports\NewProductsImport;
-use App\Imports\UpdateProductsImport;
+use App\Imports\ProductsImport;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
@@ -19,24 +18,12 @@ class ReportController extends Controller
         return view('reports.index');
     }
 
-    public function newProductImport(Request $request): RedirectResponse
+    public function productImport(Request $request): RedirectResponse
     {
         $file = $request->file('file');
 
         try {
-            Excel::import(new NewProductsImport(), $file, 'local', \Maatwebsite\Excel\Excel::CSV);
-        } catch (\Throwable $throwable) {
-            dd($throwable);
-        }
-        return redirect()->route('products.index');
-    }
-
-    public function updateProductImport(Request $request): RedirectResponse
-    {
-        $file = $request->file('file');
-
-        try {
-            Excel::queueImport(new UpdateProductsImport(), $file, 'local', \Maatwebsite\Excel\Excel::CSV);
+            Excel::queueImport(new ProductsImport(), $file, 'local', \Maatwebsite\Excel\Excel::CSV);
         } catch (\Throwable $throwable) {
             dd($throwable);
         }
