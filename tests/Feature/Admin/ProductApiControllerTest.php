9dc29024bfee2b4217f9c250a7fba5b7aedf5040
juandiego11116 <71513912+juandiego11116@users.noreply.github.com>
2022-05-15 21:44:41 -0500

    feat create tests from ProductApiController and ReporteController

---
+++ tests/Feature/Admin/ProductApiControllerTest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class ProductApiControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testGetJson():void
+    {
+        Product::factory()->count(2)->create();
+        $response = $this->getJson('api/products');
+
+        $response->assertOk()
+            ->assertJsonStructure([
+                'data' => [
+                    [
+                        'id',
+                        'name',
+                        'price',
+                        'stock_number',
+                        'id_category',
+                        'description',
+                        'photo',
+                        'visible',
+                        'created_at',
+                        'updated_at',
+                    ]
+                ]
+            ]);
+    }
+
+    public function testGetProduct():void
+    {
+        $product = Product::factory()->count(1)->create();
+        $response = $this->getJson('api/products');
+
+        $response
+            ->assertJson(fn (AssertableJson $json) =>
+            $json->has('data')
+                ->has('data.0', fn ($json) =>
+                $json->where('id', $product->toArray()[0]['id'])
+                    ->etc()
+                )
+            );
+    }
+    public function testStoreProduct():void
+    {
+        $product = Product::factory()->make();
+
+        $response = $this->postJson('api/products/store', [
+            'name' => $product->name,
+            'price' => $product->price,
+            'stock_number' => $product->stock_number,
+            'id_category' => $product->id_category,
+            'description' => $product->description,
+            'photo' => $product->photo,
+            'visible' => $product->visible,
+        ]);
+
+        $response
+            ->assertOk()
+            ->assertJson([
+                'data' => true,
+            ]);
+
+        $this->assertDatabaseHas('products', [
+            'id' => $response->json('data.id')
+        ]);
+    }
+
+    public function testUpdateProduct():void
+    {
+        $product = Product::factory()->count(1)->create();
+
+
+        $response = $this->patchJson("api/products/update/{$product->toArray()[0]['id']}", [
+            'name' => 'hi',
+            'price' => $product->toArray()[0]['price'],
+            'stock_number' => $product->toArray()[0]['stock_number'],
+            'id_category' => $product->toArray()[0]['id_category'],
+            'description' => $product->toArray()[0]['description'],
+            'photo' => $product->toArray()[0]['photo'],
+            'visible' => $product->toArray()[0]['visible'],
+        ]);
+
+        $this->assertDatabaseHas('products', [
+            'id' => $response->json('data.id'),
+            'name' => $response->json('data.name'),
+        ]);
+    }
+
+    public function testDestroyProduct():void
+    {
+        $product = Product::factory()->create();
+
+        $this->deleteJson("api/products/delete/{$product['id']}");
+
+        $this->assertDatabaseMissing('products', [
+            'id' => $product['id'],
+        ]);
+    }
+}
---
+++ tests/Feature/Admin/ReportControllerTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Feature\Admin;
+
+use App\Models\Product;
+use App\Models\User;
+use Database\Seeders\RolesTableSeeder;
+use Database\Seeders\SeederTablePermissions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\File;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Facades\Excel;
+use Tests\TestCase;
+
+class ReportControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testGoToIndex():void
+    {
+        $this->seed(SeederTablePermissions::class);
+        $this->seed(RolesTableSeeder::class);
+        $user = User::factory()->create();
+        Product::factory()->create();
+        $user->assignRole('admin');
+
+
+        $response = $this->actingAs($user)
+            ->get(route('reports.index'));
+
+        $response->assertViewIs('reports.index');
+    }
+
+    public function testImportProduct():void
+    {
+        $this->seed(SeederTablePermissions::class);
+        $this->seed(RolesTableSeeder::class);
+        $user = User::factory()->create();
+
+        $user->assignRole('admin');
+
+        Storage::fake('products');
+        $file = new File('tests\Feature\stubs\products-import.cvs');
+
+        /*$response = $this->actingAs($user)->json('POST', 'reports/import/product', [
+            'products' => UploadedFile::fake()->create('products-import.cvs')
+        ]);*/
+        $response =  $this->actingAs($user)
+            ->post(route('reports/import/product'), $file);
+        dd($response);
+        $path = base_path('tests\Feature\stubs');
+        Storage::disk('products')->assertExists($path, 'products-import.cvs');
+
+
+        /*
+
+
+        Excel::assertQueued('products-import.cvs', 'local');*/
+
+        //$path = base_path('tests\Feature\stubs\products-import.cvs');
+        //$importFile = new UploadedFile($path, 'products-import.cvs', null, null, true);
+
+        //$this->post('reports/import/product', ['file' => $importFile]);
+
+        //$this->assertDatabaseHas('products', ['name' => 'Guayaba', 'stock_number' => 1000]);
+    }
+}
